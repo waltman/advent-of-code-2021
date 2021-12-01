4a11330c7c2d4f86a5c88f388598f62cf66d546f
Walt Mankowski (macbook pro) <waltman@pobox.com>
2021-12-01 17:28:21 -0500

    C++ code for day 1

---
+++ day01/sonar_sweep.cpp
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int num_larger(vector<int> &a) {
+    int cnt = 0;
+    for (int i = 1; i < a.size(); i++)
+        if (a[i] > a[i-1])
+            cnt++;
+    return cnt;
+}
+
+int main(int argc, char *argv[]) {
+    const string fname = argv[1];
+
+    ifstream infile(fname);
+    if (!infile) {
+        perror(fname.c_str());
+        exit(1);
+    }
+
+    vector<int> depth;
+    int d;
+    while (infile >> d)
+        depth.push_back(d);
+    
+    cout << "Part 1: " << num_larger(depth) << endl;
+
+    // compute sliding windows
+    vector<int> windows;
+    for (int i = 0; i < depth.size() - 2; i++)
+        windows.push_back(depth[i] + depth[i+1] + depth[i+2]);
+    cout << "Part 2: " << num_larger(windows) << endl;
+}
