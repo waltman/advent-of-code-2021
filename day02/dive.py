89634adf13408b78a0a38c69ae07eb5f882ad542
Walt Mankowski <waltman@pobox.com>
2021-12-02 06:15:40 -0500

    refactored and added code for part 2

--- day02/dive.py
+++ day02/dive.py
@@ -1,16 +1,30 @@
 import sys
 
-d, h = 0,0
+cmds = []
 with open(sys.argv[1]) as f:
     for line in f:
-        line = line.rstrip()
-        cmd, val = line.split(' ')
-        match(cmd):
-            case 'forward':
-                h += int(val)
-            case 'down':
-                d += int(val)
-            case 'up':
-                d -= int(val)
+        cmd, val = line.rstrip().split(' ')
+        cmds.append((cmd, int(val)))
 
+d,h = 0,0
+for cmd,val in cmds:
+    match(cmd):
+        case 'forward':
+            h += val
+        case 'down':
+            d += val
+        case 'up':
+            d -= val
 print('Part 1:', d * h)
+
+d,h,a = 0,0,0
+for cmd,val in cmds:
+    match(cmd):
+        case 'forward':
+            h += val
+            d += a * val
+        case 'down':
+            a += val
+        case 'up':
+            a -= val
+print('Part 2:', d * h)
