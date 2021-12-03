f76a729451812eaea7ef27de31604ed0b618f99b
Walt Mankowski <waltman@pobox.com>
2021-12-03 06:18:13 -0500

    working on day 3 part 2. WIP

--- day03/binary_diagnostic.py
+++ day03/binary_diagnostic.py
@@ -1,12 +1,22 @@
 import sys
 import numpy as np
+import operator
+import copy
+
+def calc_rating(report, op):
+    a = copy.copy(report)
+    col = 0
+    while True:
+        half = a.shape[0] / 2
+        if op(sum(a[:,col]) half):
+            pass
 
 with open(sys.argv[1]) as f:
     report = np.array([[int(c) for c in line.rstrip()] for line in f.readlines()])
 
 gamma = ""
 eps = ""
-half = report.shape[0] // 2
+half = report.shape[0] / 2
 for i in range(report.shape[1]):
     if sum(report[:,i]) > half:
         gamma += "1"
