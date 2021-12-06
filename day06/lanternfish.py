0254a041590b2dd4036f0469e5c5a27d5ed3a40f
Walt Mankowski <waltman@pobox.com>
2021-12-06 06:15:12 -0500

    python code for day 6

---
+++ day06/lanternfish.py
@@ -0,0 +1,17 @@
+import sys
+
+fish = [0] * 9
+with open(sys.argv[1]) as f:
+    for x in map(int, f.readline().split(',')):
+        fish[x] += 1
+
+for day in range(256):
+    tmp = fish[1:]
+    tmp[6] += fish[0]
+    tmp.append(fish[0])
+    fish = [x for x in tmp]
+
+    if day == 79:
+        print('Part 1:', sum(fish))
+    elif day == 255:
+        print('Part 2:', sum(fish))
