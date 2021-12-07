894319aa89b36d591c7ba0b328acf8587627060a
Walt Mankowski <waltman@pobox.com>
2021-12-06 20:20:03 -0500

    use vectors instead of arrays

--- day06/lanternfish.cpp
+++ day06/lanternfish.cpp
@@ -1,15 +1,14 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <vector>
 #include <numeric>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
     // initialize the buckets
-    unsigned long long fish[9];
-    memset(fish, 0, sizeof(fish));
+    vector<unsigned long long> fish(9, 0);
 
     // parse the input
     const char *fname = argv[1];
@@ -26,15 +25,14 @@ int main(int argc, char *argv[]) {
 
     // now run the generations;
     for (int day = 1; day <= 256; day++) {
-        unsigned long long tmp[9];
-        memcpy(tmp, fish+1, sizeof(unsigned long long) * 8);
+        vector<unsigned long long> tmp(fish.begin()+1, fish.end());
         tmp[6] += fish[0];
-        tmp[8] = fish[0];
-        memcpy(fish, tmp, sizeof(fish));
+        tmp.push_back(fish[0]);
+        fish = tmp;
 
         if (day == 80)
-            printf("Part 1: %llu\n", accumulate(fish, fish+9, 0LL));
+            printf("Part 1: %llu\n", accumulate(fish.begin(), fish.end(), 0LL));
         else if (day == 256)
-            printf("Part 2: %llu\n", accumulate(fish, fish+9, 0LL));
+            printf("Part 2: %llu\n", accumulate(fish.begin(), fish.end(), 0LL));
     }
 }
