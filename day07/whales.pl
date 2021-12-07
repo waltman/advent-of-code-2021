3c925ebeb9f531a1f0040dc2a48fcbb7ffbb9281
Walt Mankowski <waltman@pobox.com>
2021-12-07 08:50:39 -0500

    print mean and median
    this is just for information, not for the puzzle

--- day07/whales.pl
+++ day07/whales.pl
@@ -19,11 +19,15 @@ for my $pos (min(@crabs) .. max(@crabs)) {
     my $fuel = sum map { abs $crabs[$_] - $pos } 0..$#crabs;
     ($best_pos, $best_fuel) = ($pos, $fuel) if $fuel < $best_fuel;
 }
-say "Part 1: ", $best_fuel;
+say "Part 1: ", $best_fuel, ' ', $best_pos;
 
 ($best_pos, $best_fuel) = (0, 1e300);
 for my $pos (min(@crabs) .. max(@crabs)) {
     my $fuel = sum map { cost($crabs[$_],$pos) } 0..$#crabs;
     ($best_pos, $best_fuel) = ($pos, $fuel) if $fuel < $best_fuel;
 }
-say "Part 2: ", $best_fuel;
+say "Part 2: ", $best_fuel, ' ', $best_pos;
+
+say "mean: ", sum(@crabs) / @crabs;
+my @sorted = sort {$a <=> $b} @crabs;
+say "median: $sorted[@sorted/2]";
