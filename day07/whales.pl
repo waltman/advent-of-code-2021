79c76f6fa3af75fc9b353957be3cc85b502a2b5b
Walt Mankowski <waltman@pobox.com>
2021-12-07 08:46:10 -0500

    perl code for day 7

---
+++ day07/whales.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use feature qw(:5.34);
+use experimental qw(signatures);
+use List::Util qw(sum min max);
+
+sub cost($x, $y) {
+    my $dist = abs($x-$y);
+    return $dist * ($dist + 1) / 2;
+}
+
+my @crabs = split ',', <>;
+chomp $crabs[-1];
+
+my ($best_pos, $best_fuel) = (0, 1e300);
+
+for my $pos (min(@crabs) .. max(@crabs)) {
+    my $fuel = sum map { abs $crabs[$_] - $pos } 0..$#crabs;
+    ($best_pos, $best_fuel) = ($pos, $fuel) if $fuel < $best_fuel;
+}
+say "Part 1: ", $best_fuel;
+
+($best_pos, $best_fuel) = (0, 1e300);
+for my $pos (min(@crabs) .. max(@crabs)) {
+    my $fuel = sum map { cost($crabs[$_],$pos) } 0..$#crabs;
+    ($best_pos, $best_fuel) = ($pos, $fuel) if $fuel < $best_fuel;
+}
+say "Part 2: ", $best_fuel;
