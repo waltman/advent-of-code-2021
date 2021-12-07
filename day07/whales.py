259cfc34a277948702bcbbaeb76144bf9fe18272
Walt Mankowski <waltman@pobox.com>
2021-12-07 06:06:22 -0500

    python code for day 7 part 2

--- day07/whales.py
+++ day07/whales.py
@@ -1,5 +1,9 @@
 import sys
 
+def cost(x, y):
+    dist = abs(x-y)
+    return sum(range(dist+1))
+
 with open(sys.argv[1]) as f:
     crabs = [int(x) for x in f.readline().split(',')]
 
@@ -11,4 +15,11 @@ for pos in range(min(crabs), max(crabs)):
         best_fuel = fuel
 print('Part 1:', best_fuel)
 
-        
+best_pos = 0
+best_fuel = 1e300
+for pos in range(min(crabs), max(crabs)):
+    if (fuel := sum([cost(crabs[i], pos) for i in range(len(crabs))])) < best_fuel:
+        best_pos = pos
+        best_fuel = fuel
+print('Part 2:', best_fuel)
+
