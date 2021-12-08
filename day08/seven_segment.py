d5616b304652c4ad1f54acfb5402b71bf4ebf0ec
Walt Mankowski <waltman@pobox.com>
2021-12-08 09:38:09 -0500

    python code for day 8 part 2

--- day08/seven_segment.py
+++ day08/seven_segment.py
@@ -1,14 +1,52 @@
 import sys
 import re
+from itertools import permutations
 
 cnt = 0
+tot = 0
 uniq_lens = {2, 4, 3, 7}
+DISPLAY = ['abcefg',
+           'cf',
+           'acdeg',
+           'acdfg',
+           'bcdf',
+           'abdfg',
+           'abdefg',
+           'acf',
+           'abcdefg',
+           'abcdfg',
+]
+
+def get_mapping(display, inputs):
+    for perm in permutations(range(7)):
+        remapping = {'abcdefg'[i] : 'abcdefg'[perm[i]] for i in range(7)}
+        mapping = [{remapping[c] for c in d} for d in display]
+        found = True
+        for tok in inputs:
+            tokset = {c for c in tok}
+            if tokset not in mapping:
+                found = False
+                break
+        if found:
+            return mapping
+
 with open(sys.argv[1]) as f:
     for line in f:
         m = re.match(r'(.*) \| (.*)', line.rstrip())
-        toks = m.group(2).split(' ')
-        for tok in toks:
+        inputs = m.group(1).split(' ')
+        outputs = m.group(2).split(' ')
+        for tok in outputs:
             if len(tok) in uniq_lens:
                 cnt += 1
+        mapping = get_mapping(DISPLAY, inputs)
+        val = 0
+        for tok in outputs:
+            tokset = {c for c in tok}
+            for i in range(len(mapping)):
+                if tokset == mapping[i]:
+                    val *= 10
+                    val += i
+                    break
+        tot += val
 print('Part 1:', cnt)
-
+print('Part 2:', tot)
