c767498caf32d9297a62175c9e4a564332d3c7d2
Walt Mankowski <waltman@pobox.com>
2021-12-10 06:02:38 -0500

    added code to solve day 10 part 2

--- day10/syntax_scoring.py
+++ day10/syntax_scoring.py
@@ -10,20 +10,38 @@ MATCH = {')': '(',
          ']': '[',
          '}': '{',
          '>': '<',
+         '(': ')',
+         '[': ']',
+         '{': '}',
+         '<': '>',
         }
 
+AUTOCOMPLETE_POINTS = { ')': 1,
+                        ']': 2,
+                        '}': 3,
+                        '>': 4,
+                      }
+
 error_score = 0
+autocomplete_scores = []
 with open(sys.argv[1]) as f:
     for line in f:
         stack = []
         for c in line.rstrip():
-            if c in MATCH:
+            if c in ')]}>':
                 if len(stack) == 0 or stack[-1] != MATCH[c]:
                     error_score += POINTS[c]
+                    stack = []
                     break
                 else:
                     stack.pop()
             else:
                 stack.append(c)
+        if stack:
+            score = 0
+            for c in stack[::-1]:
+                score *= 5
+                score += AUTOCOMPLETE_POINTS[MATCH[c]]
+            autocomplete_scores.append(score)
 print('Part 1:', error_score)
-
+print('Part 2:', sorted(autocomplete_scores)[len(autocomplete_scores)//2])
