c65915d32ae0dbae225b571580cef72b72a1d287
Walt Mankowski <waltman@pobox.com>
2021-12-13 06:38:18 -0500

    cleaned up the debugging code

--- day13/transparent_origami.py
+++ day13/transparent_origami.py
@@ -11,9 +11,7 @@ with open(sys.argv[1]) as f:
         if line == '':
             break
         col, row = map(int, line.split(','))
-#        print(col, row)
         grid[row,col] = True
-#    print(grid.astype(int))
 
     # handle folds
     fold = 1
@@ -21,21 +19,15 @@ with open(sys.argv[1]) as f:
         m = re.match('fold along (.)=(\d+)', line)
         axis = m.group(1)
         val = int(m.group(2))
-#        print(axis, val)
         if axis == 'x':
-#            print(grid[:,0:val].astype(int))
-#            print(np.fliplr(grid[:,val+1:].astype(int)))
-            tmp = grid[:,0:val] | np.fliplr(grid[:,val+1:])
+            grid = grid[:,0:val] | np.fliplr(grid[:,val+1:])
         else:
-#            print(grid[0:val,:].astype(int))
-#            print(np.flipud(grid[val+1:,:].astype(int)))
-            tmp = grid[0:val,:] | np.flipud(grid[val+1:,:])
-#        print(tmp.astype(int))
-        grid = tmp
+            grid = grid[0:val,:] | np.flipud(grid[val+1:,:])
         print(sum(sum(grid)))
         if fold == 1:
             print('Part 1:', sum(sum(grid)))
         fold += 1
+
 print('Part 2:')
 for row in range(grid.shape[0]):
     for col in range(grid.shape[1]):
