1e0f53e770b07ff25a945852198b0cf551ea66a2
Walt Mankowski <waltman@pobox.com>
2021-12-14 11:49:34 -0500

    removed original code for part 1

--- day14/extended_polymerization.py
+++ day14/extended_polymerization.py
@@ -1,6 +1,14 @@
 import sys
 from collections import defaultdict
 
+def compute_score(pairs):
+    counts = defaultdict(int)
+    for k in pairs:
+        for c in k:
+            if c != ' ':
+                counts[c] += pairs[k] / 2
+    return int(max(counts.values()) - min(counts.values()))
+
 # parse input
 rules = {}
 with open(sys.argv[1]) as f:
@@ -10,27 +18,15 @@ with open(sys.argv[1]) as f:
         toks = line.rstrip().split(' -> ')
         rules[toks[0]] = toks[1]
 
-# apply rules
-save_polymer = polymer
-for step in range(1,11):
-    tmp = ''
-    for i in range(len(polymer)-1):
-        tmp += polymer[i] + rules[polymer[i:i+2]]
-    polymer = tmp + polymer[-1]
-
-# compute part 1
-counts = defaultdict(int)
-for c in polymer:
-    counts[c] += 1
-print('Part 1:', max(counts.values()) - min(counts.values()))
-
-# compute part 2
+# count initial pairs
 pairs = defaultdict(int)
-for i in range(len(save_polymer)-1):
-    pairs[save_polymer[i:i+2]] += 1
-pairs[' ' + save_polymer[0]] = 1
-pairs[save_polymer[-1] + ' '] = 1
+for i in range(len(polymer)-1):
+    pairs[polymer[i:i+2]] += 1
 
+# add extra pairs for the front and back
+pairs[' ' + polymer[0]] = pairs[polymer[-1] + ' '] = 1
+
+# apply rulss
 for step in range(1,41):
     new_pairs = defaultdict(int)
     for pair in pairs:
@@ -41,10 +37,7 @@ for step in range(1,41):
         else:
             new_pairs[pair] = 1
     pairs = new_pairs
-
-counts = defaultdict(int)
-for k in pairs:
-    for c in k:
-        if c != ' ':
-            counts[c] += pairs[k] / 2
-print('Part 2:', int(max(counts.values()) - min(counts.values())))
+    if step == 10:
+        print('Part 1:', compute_score(pairs))
+    elif step == 40:
+        print('Part 2:', compute_score(pairs))
