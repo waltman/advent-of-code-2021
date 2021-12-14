c90edf318f4199a2648579d6e0f71fe5b15a0d95
Walt Mankowski <waltman@pobox.com>
2021-12-14 11:53:19 -0500

    improved variable names

--- day14/extended_polymerization.py
+++ day14/extended_polymerization.py
@@ -1,12 +1,12 @@
 import sys
 from collections import defaultdict
 
-def compute_score(pairs):
+def compute_score(num_pairs):
     counts = defaultdict(int)
-    for k in pairs:
+    for k in num_pairs.keys():
         for c in k:
             if c != ' ':
-                counts[c] += pairs[k] / 2
+                counts[c] += num_pairs[k] / 2
     return int(max(counts.values()) - min(counts.values()))
 
 # parse input
@@ -19,25 +19,25 @@ with open(sys.argv[1]) as f:
         rules[toks[0]] = toks[1]
 
 # count initial pairs
-pairs = defaultdict(int)
+num_pairs = defaultdict(int)
 for i in range(len(polymer)-1):
-    pairs[polymer[i:i+2]] += 1
+    num_pairs[polymer[i:i+2]] += 1
 
 # add extra pairs for the front and back
-pairs[' ' + polymer[0]] = pairs[polymer[-1] + ' '] = 1
+num_pairs[' ' + polymer[0]] = num_pairs[polymer[-1] + ' '] = 1
 
 # apply rulss
 for step in range(1,41):
-    new_pairs = defaultdict(int)
-    for pair in pairs:
+    new_num_pairs = defaultdict(int)
+    for pair in num_pairs.keys():
         if pair in rules:
             rule = rules[pair]
-            new_pairs[pair[0] + rule] += pairs[pair]
-            new_pairs[rule + pair[1]] += pairs[pair]
+            new_num_pairs[pair[0] + rule] += num_pairs[pair]
+            new_num_pairs[rule + pair[1]] += num_pairs[pair]
         else:
-            new_pairs[pair] = 1
-    pairs = new_pairs
+            new_num_pairs[pair] = 1
+    num_pairs = new_num_pairs
     if step == 10:
-        print('Part 1:', compute_score(pairs))
+        print('Part 1:', compute_score(num_pairs))
     elif step == 40:
-        print('Part 2:', compute_score(pairs))
+        print('Part 2:', compute_score(num_pairs))
