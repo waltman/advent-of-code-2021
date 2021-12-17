83b9f34d7787be2ec0790a47911cdef9dfc3e4ac
Walt Mankowski <waltman@pobox.com>
2021-12-17 05:47:18 -0500

    s/vi/vy/g

--- day17/trick_shot.py
+++ day17/trick_shot.py
@@ -9,16 +9,17 @@ with open(sys.argv[1]) as f:
     y1 = int(m.group(3))
     y2 = int(m.group(4))
 
+# part 1
 best_ymax = -1e300
-for vi in range(1000):
-    v = vi
+for vy in range(1000):
+    v = vy
     y = 0
     ymax = 0
     while True:
         y += v
         ymax = max(ymax, y)
         if y1 <= y <= y2:
-            print(f'Hit! {vi=} {ymax=}')
+            print(f'Hit! {vy=} {ymax=}')
             best_ymax = max(best_ymax, ymax)
             break
         elif y < y1:
@@ -26,3 +27,5 @@ for vi in range(1000):
         else:
             v -= 1
 print('Part 1:', best_ymax)
+
+# part 2
