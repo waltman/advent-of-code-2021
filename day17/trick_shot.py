d81240192973aeef402854fc20d746494565e068
Walt Mankowski <waltman@pobox.com>
2021-12-17 06:38:58 -0500

    added code to solve day 17 part 2
    also some cleanup

--- day17/trick_shot.py
+++ day17/trick_shot.py
@@ -1,6 +1,21 @@
 import sys
 import re
 
+def hit_target(x1,x2,y1,y2,vix,viy):
+    x = 0
+    vx = vix
+    y = 0
+    vy = viy
+    while True:
+        x += vx
+        vx = max(0, vx-1)
+        y += vy
+        vy -= 1
+        if x1 <= x <= x2 and y1 <= y <= y2:
+            return True
+        elif x > x2 or y < y1:
+            return False
+
 # parse input
 with open(sys.argv[1]) as f:
     m = re.search(r'x=([\d\-]+)\.\.([\d\-]+), y=([\d\-]+)\.\.([\d\-]+)', f.readline())
@@ -19,7 +34,6 @@ for vy in range(1000):
         y += v
         ymax = max(ymax, y)
         if y1 <= y <= y2:
-            print(f'Hit! {vy=} {ymax=}')
             best_ymax = max(best_ymax, ymax)
             break
         elif y < y1:
@@ -29,3 +43,9 @@ for vy in range(1000):
 print('Part 1:', best_ymax)
 
 # part 2
+cnt = 0
+for vix in range(x2+1):
+    for viy in range(y1,500):
+        if hit_target(x1,x2,y1,y2,vix,viy):
+            cnt += 1
+print('Part 2:', cnt)
