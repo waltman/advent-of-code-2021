26e22eea36ff23a262dd563c890f114e515e720b
Walt Mankowski <waltman@pobox.com>
2021-12-19 10:40:09 -0500

    I think this is FINALLY handling explode properly!

--- day18/Pair.py
+++ day18/Pair.py
@@ -14,22 +14,45 @@ class Pair:
     def is_leaf(self):
         return isinstance(self.left, int) and isinstance(self.right, int)
 
+    # Mark -- "My explode "closest" left function was to go back up
+    # the tree until I could go left once, then I go right until a
+    # terminal node, it seems to have worked
     def reg_left(self):
-        p = self.parent
-        while p and p.is_left_child():
+        p = self
+        while p and (p.is_left_child() or p.is_root()):
             p = p.parent
-        if p.parent is None:
-            if isinstance(p.left, int):
-                return p
-            else:
-                return None
-        p = p.parent
-        while p and isinstance(p.left, Pair):
-            p = p.left
-        if p:
-            return p
+        if p is None:
+            return None, None
         else:
-            return None
+            if isinstance(p.parent.left, int):
+                print('found left:', p.parent.left)
+                return p.parent, 'left'
+            else:
+                p = p.parent.left
+                while p:
+                    if isinstance(p.right, int):
+                        print('found left:', p.right)
+                        return p, 'right'
+                    else:
+                        p = p.right
+                return None, None
+        # now we're either the root or a right child
+
+        # p = self.parent
+        # while p and p.is_left_child():
+        #     p = p.parent
+        # if p.parent is None:
+        #     if isinstance(p.right, int):
+        #         return p
+        #     else:
+        #         return None
+        # p = p.parent
+        # while p and isinstance(p.left, Pair):
+        #     p = p.left
+        # if p:
+        #     return p
+        # else:
+        #     return None
         # if p.is_right_child():
         #     p = p.parent
         # while p and p.is_left_child():
@@ -49,21 +72,39 @@ class Pair:
         # return None
         
     def reg_right(self):
-        p = self.parent
-        while p and p.is_right_child():
+        p = self
+        while p and (p.is_right_child() or p.is_root()):
             p = p.parent
-        if p.parent is None:
-            if isinstance(p.right, int):
-                return p
-            else:
-                return None
-        p = p.parent
-        while p and isinstance(p.right, Pair):
-            p = p.right
-        if p:
-            return p
+        if p is None:
+            return None, None
         else:
-            return None
+            if isinstance(p.parent.right, int):
+                print('found right:', p.parent.right)
+                return p.parent, 'right'
+            else:
+                p = p.parent.right
+                while p:
+                    if isinstance(p.left, int):
+                        print('found right:', p.left)
+                        return p, 'left'
+                    else:
+                        p = p.left
+                return None, None
+        # p = self.parent
+        # while p and p.is_right_child():
+        #     p = p.parent
+        # if p.parent is None:
+        #     if isinstance(p.left, int):
+        #         return p
+        #     else:
+        #         return None
+        # p = p.parent
+        # while p and isinstance(p.right, Pair):
+        #     p = p.right
+        # if p:
+        #     return p
+        # else:
+        #     return None
         # if p.is_left_child():
         #     p = p.parent
         # while p and p.is_right_child():
@@ -88,6 +129,9 @@ class Pair:
     def is_right_child(self):
         return self.parent and self.parent.right == self
 
+    def is_root(self):
+        return self.parent is None
+
     def explodable(self):
         stack = [(self, 0)]
         while len(stack):
@@ -101,14 +145,22 @@ class Pair:
         return None
 
     def explode(self):
-        if p := self.reg_left():
-            p.left += self.left
+        p,lr = self.reg_left()
+        if p:
+            if lr == 'left':
+                p.left += self.left
+            else:
+                p.right += self.left
             # if isinstance(p.right, int):
             #     p.right += self.left
             # else:
             #     p.left += self.left
-        if p := self.reg_right():
-            p.right += self.right
+        p,lr = self.reg_right()
+        if p:
+            if lr == 'left':
+                p.left += self.right
+            else:
+                p.right += self.right
             # if isinstance(p.left, int):
             #     p.left += self.right
             # else:
