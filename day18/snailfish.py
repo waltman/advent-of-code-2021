464cb62397fb27aad10e844e2276b0dafd0a3cbb
Walt Mankowski <waltman@pobox.com>
2021-12-19 11:39:28 -0500

    python code to solve day 18 part 2

--- day18/snailfish.py
+++ day18/snailfish.py
@@ -29,7 +29,27 @@ for i in range(1, len(lines)):
     reduce(pair)
     print('reduced:', pair)
 
-print(pair.magnitude())
+print('Part 1:', pair.magnitude())
+
+best_mag = -1
+tests = 0
+for i in range(len(lines)):
+    l1 = lines[i]
+    pair = Pair(eval(l1))
+    reduce(pair)
+    save_pair = str(pair)
+    for j in range(len(lines)):
+        if i == j:
+            continue
+        l2 = lines[j]
+        pairstr = f'[{save_pair},{l2}]'
+        pair = Pair(eval(pairstr))
+        reduce(pair)
+        mag = pair.magnitude()
+        best_mag = max(best_mag, mag)
+        tests += 1
+print('Part 2:', best_mag)
+print(tests)
 
 # pair_str = lines[0]
 # for s in lines[1:]:
