57013359355e93d94cfe202d9793a7535b50ac9b
Walt Mankowski <waltman@pobox.com>
2021-12-21 18:32:10 -0500

    fixed code to solve part 1

--- day21/dirac_dice.py
+++ day21/dirac_dice.py
@@ -8,11 +8,11 @@ class D100:
     def roll(self):
         val = 0
         for _ in range(3):
-            val = self.d100
+            val += self.d100
             self.d100 += 1
             if self.d100 == 101:
                 self.d100 = 1
-        self.rolls += 1
+        self.rolls += 3
         return val
 
 # parse input
@@ -23,17 +23,19 @@ with open(sys.argv[1]) as f:
 d100 = D100()
 ps1 = 0
 ps2 = 0
+board = [x for x in range(1,11)]
 
 while True:
     p1 = (p1 + d100.roll()) % 10
-    ps1 += p1 + 1
+    ps1 += board[p1]
     if ps1 >= 1000:
+        print(ps1, ps2, d100.rolls)
+        print('Part 1:', ps2 * d100.rolls)
         break
 
     p2 = (p2 + d100.roll()) % 10
-    ps2 += p2 + 1
+    ps2 += board[p2]
     if ps2 >= 1000:
+        print(ps1, ps2, d100.rolls)
+        print('Part 1:', p12 * d100.rolls)
         break
-
-    break
-print(p1, ps1, p2, ps2)
