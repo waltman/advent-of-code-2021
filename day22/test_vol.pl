4b69cd402807e5a8b36b9fda0eb8fc5b83a57708
Walt Mankowski <waltman@pobox.com>
2021-12-23 05:49:01 -0500

    more experiments with volume

--- day22/test_vol.pl
+++ day22/test_vol.pl
@@ -6,14 +6,24 @@ use experimental qw(signatures);
 
 # let's see what the total volume is here...
 my $tot = 0;
+my ($minx, $maxx, $miny, $maxy, $minz, $maxz) = (1e300, -1e300, 1e300, -1e300, 1e300, -1e300);
 while (<>) {
     chomp;
     m/x=([\-\d]+)\.\.([\-\d]+),y=([\-\d]+)\.\.([\-\d]+),z=([\-\d]+)\.\.([\-\d]+)/;
-    say $_;
-    say "$1 $2 $3 $4 $5 $6";
+#    say $_;
+#    say "$1 $2 $3 $4 $5 $6";
     my $vol = ($2 - $1 + 1) * ($4 - $3 + 1) * ($6 - $5 + 1);
-    say $vol;
+#    say $vol;
     $tot += $vol;
+    $minx = $1 if $1 < $minx;
+    $miny = $1 if $1 < $miny;
+    $minz = $1 if $1 < $minz;
+    $maxx = $1 if $1 > $maxx;
+    $maxy = $1 if $1 > $maxy;
+    $maxz = $1 if $1 > $maxz;
 }
 
 say $tot;
+say "x: $minx $maxx ", $maxx-$minx+1;
+say "y: $miny $maxy ", $maxy-$miny+1;
+say "z: $minz $maxz ", $maxz-$minz+1;
