3f27ac9bb4b67f791c5100a65d66ac8c9ef3550f
Walt Mankowski <waltman@pobox.com>
2021-12-23 14:11:31 -0500

    getting close but died on oom error
    z=8913

--- day22/reactor_reboot2.py
+++ day22/reactor_reboot2.py
@@ -26,13 +26,27 @@ with open(sys.argv[1]) as f:
 
 rules2 = [(action, x1-minx, x2-minx+1, y1-miny, y2-miny+1, z1, z2) for (action, x1, x2, y1, y2, z1, z2) in rules]
 cnt = 0
+cache = dict()
+RES = 2758514936282235
 for z in range(minz, maxz+1):
-    mat = np.zeros([maxx-minx+1,maxy-miny+1], bool)
-    num_ons = 0
-    num_offs = 0
-    onsize =0
-    for (action, x1, x2, y1, y2, z1, z2) in rules2:
+    # check with rules apply
+    valid_rules = []
+    for i in range(len(rules2)):
+        (action, x1, x2, y1, y2, z1, z2) = rules2[i]
         if z1 <= z <= z2:
+            valid_rules.append(i)
+    valid_rules = tuple(valid_rules)
+
+    # have we already solved this one?
+    if valid_rules in cache:
+        cnt += cache[valid_rules]
+    else:
+        mat = np.zeros([maxx-minx+1,maxy-miny+1], bool)
+        num_ons = 0
+        num_offs = 0
+        onsize = 0
+        for i in valid_rules:
+            (action, x1, x2, y1, y2, z1, z2) = rules2[i]
             print(action, x1, x2, y1, y2, z1, z2)
             if action == 'on':
                 mat[x1:x2, y1:y2] = 1
@@ -41,10 +55,13 @@ for z in range(minz, maxz+1):
             else:
                 mat[x1:x2, y1:y2] = 0
                 num_offs += 1
-    if num_ons == 1 and num_offs == 0:
-        cnt += onsize
-    else:
-        cnt += np.count_nonzero(mat)
-    print(z, cnt, onsize, num_ons, num_offs)
+        if num_ons == 1 and num_offs == 0:
+            cache[valid_rules] = onsize
+            cnt += cache[valid_rules]
+        elif num_ons > 0:
+            cache[valid_rules] = np.count_nonzero(mat)
+            cnt += cache[valid_rules]
+
+    print(z, cnt, onsize, num_ons, num_offs, RES-cnt)
 
 print('Part 2:', cnt)
