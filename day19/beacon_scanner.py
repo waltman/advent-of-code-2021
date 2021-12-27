2c343d0fd528d40a9f26d90a89c043a10d408ca0
Walt Mankowski <waltman@pobox.com>
2021-12-27 07:11:47 -0500

    My last lame attempt
    This is the point where my code was when I gave up last night and went
    to the subreddit for help.  Looking at jimcasey's code I see this was
    never going to work because I wasn't handling the rotations properly.
    
    Also, making triangles with the two nearest points and using their
    areas as an invariant is a really clever idea. I was getting there
    with my initial pairwise distance idea, but this way is better.

--- day19/beacon_scanner.py
+++ day19/beacon_scanner.py
@@ -1,5 +1,6 @@
 import sys
 from itertools import permutations, product
+from collections import defaultdict
 
 def orientations():
     for perm in permutations(range(3)):
@@ -9,12 +10,34 @@ def orientations():
 # generate set of intrabeacon offsets
 def gen_offsets(coords):
     offsets = set()
+    pairs = dict()
     for i in range(len(coords)-1):
         for j in range(i+1, len(coords)):
-            offsets.add((abs(coords[i][0] - coords[j][0]),
-                         abs(coords[i][1] - coords[j][1]),
-                         abs(coords[i][2] - coords[j][2])))
-    return offsets
+            vals = (coords[i][0] - coords[j][0],
+                    coords[i][1] - coords[j][1],
+                    coords[i][2] - coords[j][2])
+            offsets.add(vals)
+            pairs[vals] = (i,j)
+    return offsets, pairs
+
+# def match_offsets(matched, scanner):
+#     for x,y,z,p1,p2 in orientations():
+#         # find the offsets and pairs for this orientation
+#         coords = [(arr[x], arr[y] * p1, arr[z] * p2) for arr in scanner]
+#         offsets, pairs = gen_offsets(coords)
+
+#         # check if the offsets match anything we're already found
+#         for match in matched:
+#             good_idx, good_offsets, good_pairs = match
+#             if (good_offsets & offsets):
+#                 overlap = set()
+#                 for triple in good_offsets & offsets:
+#                     for offset in pairs[triple]:
+#                         overlap.add(offset)
+#                 if len(overlap) >= 12:
+#                     # convert the overlap
+#                     return good_idx, overlap, x, y, z, p1, p2
+
 
 # parse input
 scanners = []
@@ -31,8 +54,98 @@ with open(sys.argv[1]) as f:
             coords.append([int(x) for x in line.split(',')])
 scanners.append(coords)
 
-s0 = gen_offsets(scanners[0])
-for x,y,z,p1,p2 in orientations():
-    coords = [(arr[x], arr[y] * p1, arr[z] * p2) for arr in scanners[1]]
-    s1 = gen_offsets(coords)
-    print(len(s0 & s1))
+unmatched = {i for i in range(1, len(scanners))}
+matched = {0}
+
+offsets, pairs = gen_offsets(scanners[0])
+#matched = [(0, offsets, pairs)] 
+beacons = set()
+deltas = {0: (0,0,0)}
+orients = {0: (0,1,2,1,1)}
+
+while unmatched:
+    to_remove = set()
+    for idx1 in unmatched:
+        sc1 = scanners[idx1]
+        done = False
+        for idx0 in matched:
+            if done:
+                break
+            print(f'testing {idx0} vs {idx1}')
+            sc0 = scanners[idx0]
+            gx,gy,gz,gp1,gp2 = orients[idx0]
+            for x,y,z,p1,p2 in orientations():
+                if done:
+                    break
+                cnt = defaultdict(list)
+                for t0 in sc0:
+                    for t1 in sc1:
+#                        trip = (t1[x] + t0[gx], t1[y] * p1 + t0[gy] * gp1, t1[z] * p2 + t0[gz] * gp2)
+                        trip = (t1[x] + t0[gx], t1[y] * p1 + t0[gy],  t1[z] * p2 + t0[gz])
+                        cnt[trip].append((t0[0], t0[1], t0[2]))
+                for k,v in cnt.items():
+                    if len(v) >= 12:
+                        print(k, v)
+                        to_remove.add(idx1)
+                        deltas[idx1] = k
+                        orients[idx1] = (x,y,z,p1,p2)
+                        done = True
+    if not to_remove:
+        print("nothing to remove!")
+        break
+    unmatched -= to_remove
+    for x in to_remove:
+        matched.add(x)
+#        dx,dy,dz = deltas[x]
+#        normalized_scan = [(x + dx, y+dy, z+dz) for x,y,z in scanners[x]]
+#        scanners[x] = normalized_scan
+
+# sc0 = scanners[0]
+# sc1 = scanners[1]
+# for x,y,z,p1,p2 in orientations():
+#     cnt = defaultdict(list)
+#     for t0 in sc0:
+#         for t1 in sc1:
+#             trip = (t1[x] + t0[0], t1[y] * p1 + t0[1], t1[z] * p2 + t0[2])
+#             cnt[trip].append((t0[0], t0[1], t0[2]))
+#         for k,v in cnt.items():
+#             if len(v) >= 12:
+#                 print(k, v)
+#                 break
+# #        print(cnt)
+
+# while unmatched:
+#     to_remove = set()
+#     for idx in unmatched:
+#         for x,y,z,p1,p2 in orientations():
+#             # find the offsets and pairs for this orientation
+#             coords = [(arr[x], arr[y] * p1, arr[z] * p2) for arr in scanners[idx]]
+#             offsets, pairs = gen_offsets(coords)
+
+#             # check if the offsets match anything we're already found
+#             for match in matched:
+#                 good_idx, good_offsets, good_pairs = match
+#                 if (good_offsets & offsets):
+#                     overlap = set()
+#                     for triple in good_offsets & offsets:
+#                         for offset in pairs[triple]:
+#                             overlap.add(offset)
+#                     if len(overlap) >= 12:
+#                         # get the location of scanner idx relative to scanner good_idx
+#                         triple = list(good_offsets & offsets)[0]
+#                         good_offset = good_pairs[triple]
+#                         new_offset = pairs[triple]
+#         break
+#     break
+
+# for x,y,z,p1,p2 in orientations():
+#     coords = [(arr[x], arr[y] * p1, arr[z] * p2) for arr in scanners[1]]
+#     s1,off1 = gen_offsets(coords)
+#     print(len(s0 & s1))
+#     overlap = set()
+#     if (len(s0 & s1) >= 28):
+#         for triple in s0 & s1:
+#             for offset in off0[triple]:
+#                 overlap.add(offset)
+#         print(overlap)
+#         print(x,y,z,p1,p2)
