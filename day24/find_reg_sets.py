b30c9acf00980cfb5d06c0e8e71db616a7635f35
Walt Mankowski <waltman@pobox.com>
2022-01-02 13:38:19 -0500

    for each new digit only search the tuples that occurred least frequently
    Output:
    
    % time python find_reg_sets.py
    i=0 len(reg_set)=9
    i=1 len(reg_set)=81
    i=2 len(reg_set)=729
    i=3 len(reg_set)=6561
    i=4 len(reg_set)=8019
    i=5 len(reg_set)=1458
    i=6 len(reg_set)=729
    i=7 len(reg_set)=1134
    i=8 len(reg_set)=117
    i=9 len(reg_set)=16
    i=10 len(reg_set)=9
    i=11 len(reg_set)=12
    i=12 len(reg_set)=9
    i=13 len(reg_set)=17
    (2, 0, 0, 0)
    (3, 0, 0, 0)
    (4, 0, 0, 0)
    (5, 0, 0, 0)
    (6, 0, 0, 0)
    (7, 0, 0, 0)
    (8, 0, 0, 0)
    (9, 0, 0, 0)
    python find_reg_sets.py  3.27s user 0.06s system 100% cpu 3.333 total

--- day24/find_reg_sets.py
+++ day24/find_reg_sets.py
@@ -6,13 +6,13 @@ from collections import defaultdict
 with open('input.txt') as f:
     pgm = [line.rstrip() for line in f]
 
-regsets = [set([(0,0,0,0)])]
+regsets = [(0,0,0,0)]
 
 for i in range(14):
-    reg_set = set()
+    reg_set = defaultdict(int)
     for pr in range(1, 10):
         monad = Monad(pgm[i*18:(i+1)*18], [pr])
-        for regs in regsets[i]:
+        for regs in regsets:
             monad.reset()
             monad.reg['w'] = regs[0]
             monad.reg['x'] = regs[1]
@@ -20,10 +20,19 @@ for i in range(14):
             monad.reg['z'] = regs[3]
             monad.run()
             k = (monad.reg['w'],monad.reg['x'],monad.reg['y'],monad.reg['z'])
-            reg_set.add(k)
+            reg_set[k] += 1
     print(f'{i=} {len(reg_set)=}')
-    regsets.append(reg_set)
+    # find the tuples that occurred least frequently
+    best_cnt = float('Inf')
+    best_tuples = []
+    for k,v in reg_set.items():
+        if v < best_cnt:
+            best_cnt = v
+            best_tuples = [k]
+        elif v == best_cnt:
+            best_tuples.append(k)
+    regsets = best_tuples
 
-for regs in regsets[-1]:
+for regs in regsets:
     if regs[3] == 0:
         print(regs)
