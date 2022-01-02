3c4a6e3791524ffd4b7daf0ce682b28c1887b9f8
Walt Mankowski <waltman@pobox.com>
2022-01-02 14:06:08 -0500

    it works!
    This solves both parts in under 4 seconds!

--- day24/find_reg_sets.py
+++ day24/find_reg_sets.py
@@ -6,33 +6,43 @@ from collections import defaultdict
 with open('input.txt') as f:
     pgm = [line.rstrip() for line in f]
 
-regsets = [(0,0,0,0)]
+regsets = [(0,0,0,0,[])]
 
 for i in range(14):
     reg_set = defaultdict(int)
+    prs = defaultdict(list)
     for pr in range(1, 10):
         monad = Monad(pgm[i*18:(i+1)*18], [pr])
-        for regs in regsets:
+        for w,x,y,z,arr in regsets:
             monad.reset()
-            monad.reg['w'] = regs[0]
-            monad.reg['x'] = regs[1]
-            monad.reg['y'] = regs[2]
-            monad.reg['z'] = regs[3]
+            monad.reg['w'] = w
+            monad.reg['x'] = x
+            monad.reg['y'] = y
+            monad.reg['z'] = z
             monad.run()
+            if len(arr) == 0:
+                new_arr = [pr]
+            else:
+                new_arr = [x * 10 + pr for x in arr]
             k = (monad.reg['w'],monad.reg['x'],monad.reg['y'],monad.reg['z'])
             reg_set[k] += 1
+            prs[k] += new_arr
     print(f'{i=} {len(reg_set)=}')
     # find the tuples that occurred least frequently
     best_cnt = float('Inf')
     best_tuples = []
     for k,v in reg_set.items():
+        w,x,y,z = k
         if v < best_cnt:
             best_cnt = v
-            best_tuples = [k]
+            best_tuples = [(w,x,y,z,prs[k])]
         elif v == best_cnt:
-            best_tuples.append(k)
+            best_tuples.append((w,x,y,z,prs[k]))
     regsets = best_tuples
 
+valid = []
 for regs in regsets:
     if regs[3] == 0:
-        print(regs)
+        valid += regs[4]
+print('Part 1:', sorted(valid)[-1])
+print('Part 2:', sorted(valid)[0])
